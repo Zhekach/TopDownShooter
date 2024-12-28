e0a869ef3b45e1471ac0da0e4e0c24cdfa466cc2
Zhekach <62952369+Zhekach@users.noreply.github.com>
2024-12-28 19:19:46 +0300

    refactor spawncontroller minor

--- Assets/Scripts/Map/SpawnController.cs
+++ Assets/Scripts/Map/SpawnController.cs
@@ -18,15 +18,15 @@ namespace Map
 
         private void OnEnable()
         {
-            Bullet.OnBulletHit += SpawnMap;
+            Bullet.OnBulletHit += RespawnMap;
         }
 
         private void OnDisable()
         {
-            Bullet.OnBulletHit -= SpawnMap;
+            Bullet.OnBulletHit -= RespawnMap;
         }
 
-        private void SpawnMap(Bullet bullet = null)
+        private void SpawnMap()
         {
             _player = _playerSpawner.Spawn();
             _enemy = _enemySpawner.Spawn();
@@ -36,6 +36,12 @@ namespace Map
             
             var enemyController = _enemy.GetComponent<EnemyController>();
             enemyController.Initialize(_player);
+        }
+
+        private void RespawnMap(Bullet bullet = null)
+        {
+            _player.transform.position = _playerSpawner.transform.position;
+            _enemy.transform.position = _enemySpawner.transform.position;
         }
     }
 }
